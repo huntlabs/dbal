f18b890254b6c3ad5620c990d3cde56954539c72
viile <dakgzhu@foxmail.com>
2017-08-24 03:44:21 -0400

    add sqlite support

--- source/dbal/package.d
+++ source/dbal/package.d
@@ -16,3 +16,6 @@ public import dbal.driver.mysql.builder;
 
 public import dbal.driver.pgsql.syntax;
 public import dbal.driver.pgsql.builder;
+
+public import dbal.driver.sqlite.syntax;
+public import dbal.driver.sqlite.builder;
